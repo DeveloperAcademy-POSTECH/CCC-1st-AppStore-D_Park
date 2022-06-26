745a35a8485dbe011c524b17e0b7ad2dc420f398
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2022-06-27 01:34:12 +0900

    ✨ : 크기 상수 추가

---
+++ AppStoreClone/AppStoreClone/Model/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  AppStoreClone
+//
+//  Created by JungHoonPark on 2022/06/27.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    static var screenWidth: CGFloat {
+        UIScreen.main.bounds.width
+    }
+    static var screenHeight: CGFloat {
+        UIScreen.main.bounds.height
+    }
+}
