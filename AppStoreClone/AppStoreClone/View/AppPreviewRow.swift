416f0817c0c2234cf9663a8e1d029dd407b859c9
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2022-06-27 02:24:34 +0900

    :sparkles: : app탭 가로 프리뷰 레이아웃

---
+++ AppStoreClone/AppStoreClone/View/AppPreviewRow.swift
@@ -0,0 +1,45 @@
+//
+//  AppPreviewRow.swift
+//  AppStoreClone
+//
+//  Created by JungHoonPark on 2022/06/27.
+//
+
+import SwiftUI
+
+struct AppPreviewRow: View {
+    var body: some View {
+        HStack {
+            AsyncImage(url: URL(string: "https://source.unsplash.com/random")) { image in
+                image.resizable()
+            } placeholder: {
+                ProgressView()
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .frame(width: 79, height: 77)
+                VStack(alignment: .leading) {
+                    Text("Title")
+                        .font(.system(size: 20, weight: .semibold))
+                    Text("SubTitle")
+                        .font(.system(size: 13, weight: .semibold))
+                        .foregroundColor(.gray)
+                }
+                .padding(.bottom, 2)
+                Spacer()
+                Button("Get") {
+                    print("Download pressed!")
+                }
+                .padding()
+                .frame(width: 60, height: 27)
+                .background(Color.yellow)
+                .clipShape(Capsule())
+        }
+        .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+    }
+}
+
+struct AppPreviewRow_Previews: PreviewProvider {
+    static var previews: some View {
+        AppPreviewRow()
+    }
+}
---
+++ AppStoreClone/AppStoreClone/View/AppTab/AppView.swift
@@ -0,0 +1,21 @@
+//
+//  AppView.swift
+//  AppStoreClone
+//
+//  Created by JungHoonPark on 2022/06/27.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+
+        AppPreviewRow()
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
