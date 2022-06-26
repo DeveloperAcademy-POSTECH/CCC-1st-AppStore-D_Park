6708b1563323281b4d44197da25b78e575b45845
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2022-06-27 02:24:46 +0900

    :sparkles: : 탭뷰 세팅

--- AppStoreClone/AppStoreClone/ContentView.swift
+++ AppStoreClone/AppStoreClone/ContentView.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+              Text("Today Tab")
+                .tabItem {
+                  Image(systemName: "newspaper")
+                  Text("Today")
+                }
+              Text("Game Tab")
+                .tabItem {
+                  Image(systemName: "gamecontroller")
+                  Text("Game")
+                }
+            AppView()
+                .tabItem {
+                Image(systemName: "square.stack.3d.up.fill")
+                Text("App")
+              }
+            Text("Aracade Tab")
+              .tabItem {
+                Image(systemName: "externaldrive.connected.to.line.below.fill")
+                Text("Aracade")
+              }
+              Text("Search Tab")
+                .tabItem {
+                  Image(systemName: "magnifyingglass")
+                  Text("Search")
+                }
+            }
+            .font(.headline)
+            .edgesIgnoringSafeArea(.bottom)
     }
 }
 
