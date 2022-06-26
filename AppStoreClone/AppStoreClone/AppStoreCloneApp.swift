e621b125ccb3fcfe5eb6f8158c614a6f4c36298b
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2022-06-26 22:57:53 +0900

    🎉 : 앱스토어 클론 초기세팅
    - SwiftLint 설정
    - .gitignore 설정

---
+++ AppStoreClone/AppStoreClone/AppStoreCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreCloneApp.swift
+//  AppStoreClone
+//
+//  Created by JungHoonPark on 2022/06/26.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
